3732b10f6fd9eb417e7fc00129133e99102300ab
dsjacob20 <90866861+dsjacob20@users.noreply.github.com>
2021-12-16 13:13:06 -0700

    Add files via upload

---
+++ index6/main6(1).js
@@ -0,0 +1,19 @@
+function ja(){
+open('index6a.html', '_self');
+}
+
+function jb(){
+open('index6b.html', '_self');
+}
+
+function jc(){
+open('index6c.html', '_self');
+}
+function jd(){
+open('index6d.html', '_self');
+}
+
+function je(){
+open('index6e.html', '_self');
+}
+
